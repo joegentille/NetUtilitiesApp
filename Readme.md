fab02ed6e421c41861208cde14a9e7d2b33e95df
joegentille <joegentille@gmail.com>
2021-01-26 17:10:03 -0500

    First commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# UtilityWrapper
+
+## 1. First Test for this project
+
+- So this is a test project for creating a pipeline
+
---
+++ UtilityWrapper/UtilityWrapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UtilityWrapper
+{
+    public class UtilityWrapper
+    {
+
+        public void DoSomething()
+        {
+            var name = "Utility";
+            if(string.IsNullOrEmpty(name))
+            {
+                //
+            }
+        }
+
+        public void DoSomethingElse()
+        {
+            var name = "UtilityElse";
+            if (string.IsNullOrEmpty(name))
+            {
+                //
+            }
+        }
+
+    }
+}
